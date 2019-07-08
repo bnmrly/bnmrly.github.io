efac2da7ac6d2ea8e29fd585dcd8385ec7f59ad2
Ben Marley <benjaminjohnmarley@gmail.com>
2019-07-08 21:10:53 +0100

    Ad bg color

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -4,13 +4,11 @@ const GlobalStyle = createGlobalStyle`
 
 @import url('https://fonts.googleapis.com/css?family=Montserrat:100,200,300,400,500,600,700,800,900&display=swap');
 
-html {
-  background-color: ${props => props.theme.color.siteBackgroundColor}
-}
 
 body {
   font-family: 'Montserrat', sans-serif;
-  color: ${props => props.theme.color.primaryFontColor}
+  color: ${props => props.theme.color.primaryFontColor};
+  background-color: ${props => props.theme.color.siteBackground};
 }
 
 h1 {
