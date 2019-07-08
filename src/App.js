f54b6702d5cd5976396de3ab8c32b1f0d8d6da81
Ben Marley <benjaminjohnmarley@gmail.com>
2019-07-08 20:11:04 +0100

    Add normalize

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import styled from 'styled-components/macro';
+import { Normalize } from 'styled-normalize';
+
+const AppWrapper = styled.div`
+  margin: 1rem;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello world</h1>
-    </div>
+    <Fragment>
+      <Normalize />
+      <AppWrapper>
+        <h1>hello world</h1>
+      </AppWrapper>
+    </Fragment>
   );
 }
 
