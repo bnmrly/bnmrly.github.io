7f2c755605c835d32ee3e31cab018304a73ab6bd
Ben Marley <benjaminjohnmarley@gmail.com>
2019-07-17 20:22:17 +0100

    Tweak colours

--- src/Theme.js
+++ src/Theme.js
@@ -1,8 +1,10 @@
 const Theme = {
   color: {
-    siteBackground: '#d9d9d9',
+    // siteBackground: '#d9d9d9',
+    siteBackground: '#FDF9F8',
     primaryFont: '#333',
     link: '#b00090',
+    // link: '#27B095',
     linkHover: '#C57EBC'
   },
   dimensions: {
