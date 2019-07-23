7b66ec69aca0717d078b6c674f258c3f7009f482
Ben Marley <benjaminjohnmarley@gmail.com>
2019-07-23 20:17:51 +0100

    Update theme dimensions and styles

--- src/Theme.js
+++ src/Theme.js
@@ -5,11 +5,20 @@ const Theme = {
     primaryFont: '#333',
     link: '#b00090',
     // link: '#27B095',
-    linkHover: '#C57EBC'
+    linkHover: '#C57EBC',
+    border: '#99ead4'
   },
   dimensions: {
     iconHeight: '40px',
-    letterSpacingOverride: '0.2rem'
+    letterSpacingOverride: '0.2rem',
+    appWrapperMarginY: '2rem',
+    appWrapperMarginX: '3rem',
+    headerMarginY: '2rem',
+    borderRadius: '0.5rem',
+    defaultSectionPadding: '2rem',
+    defaultSectionMarginY: '5rem',
+    lastSectionMarginY: '2rem',
+    copyrightFontSize: '0.75rem'
   }
 };
 
