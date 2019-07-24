f6a42e1feaf73f893d5b6f9f241405ccc34a993d
Ben Marley <benjaminjohnmarley@gmail.com>
2019-07-24 21:27:02 +0100

    Add imageUrls to project data

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -3,49 +3,57 @@ const projectData = [
     name: 'News Today: Front End',
     link: 'https://news-today-ben-marley.herokuapp.com/',
     description:
-      'Front End for my final full stack solo project for the Northcoders coding bootcamp. Built with React.js and vanilla css.'
+      'Front End for my final full stack solo project for the Northcoders coding bootcamp. Built with React.js and vanilla css.',
+    imageUrl: './projectImages/news-today-fe.png'
   },
   {
     name: 'News Today: Back End',
     link: 'https://ben-nc-news.herokuapp.com/API',
     description:
-      'RESTful API built with Node.js, Express.js, MongoDB and Mongoose.'
+      'RESTful API built with Node.js, Express.js, MongoDB and Mongoose.',
+    imageUrl: './projectImages/news-today-be.png'
   },
   {
     name: 'CreditWise',
     link: 'https://github.com/bnmrly/project-game-FE',
     description:
-      'Financial Education Progressive Web App game for Key Stage 2 children. Built in a group of six, with Node.js, React.js, Redux and Firebase.'
+      'Financial Education Progressive Web App game for Key Stage 2 children. Built in a group of six, with Node.js, React.js, Redux and Firebase.',
+    imageUrl: './projectImages/creditWise.png'
   },
   {
     name: 'Google Books API Data Visualisation',
     link: 'https://tranquil-hollows-48024.herokuapp.com/',
     description:
-      'React app that calls the Google Books API to retrieve book data. Built with React.js and react-chartjs-2.'
+      'React app that calls the Google Books API to retrieve book data. Built with React.js and react-chartjs-2.',
+    imageUrl: './projectImages/google-books-api.png'
   },
   {
     name: 'Street Support Network News Site',
     link: 'https://news.streetsupport.net',
     description:
-      'WordPress site for the Manchester homelessness start up. Built with Sass, HTML5 and PHP.'
+      'WordPress site for the Manchester homelessness start up. Built with Sass, HTML5 and PHP.',
+    imageUrl: './projectImages/street-support-project.png'
   },
   {
     name: "Illustrator's website",
     link: 'http://jimnewcombe.co.uk',
     description:
-      'Responsive, custom WordPress site for the London based artist and writer, Jim Newcombe'
+      'Responsive, custom WordPress site for the London based artist and writer, Jim Newcombe',
+    imageUrl: './projectImages/jim-newcombe-project.png'
   },
   {
     name: 'no-wave: Mouseover experiment',
     link: 'http://benmarley.co.uk/no-wave/',
     description:
-      'Dom Mouseover experiment built using CSS Grid and vanilla JavaScript.'
+      'Dom Mouseover experiment built using CSS Grid and vanilla JavaScript.',
+    imageUrl: './projectImages/no-wave-project.png'
   },
   {
     name: 'WordPress Photography Theme',
     link: 'http://benmarleyphotography.co.uk',
     description:
-      'Responsive WordPress site of my film photography, built with the underscores starter theme'
+      'Responsive WordPress site of my film photography, built with the underscores starter theme',
+    imageUrl: './projectImages/bm-photo-project.png'
   }
 ];
 
