5cfc4e9992f98a2f11e7eda3877bab1540111267
Ben Marley <benjaminjohnmarley@gmail.com>
2019-08-23 21:07:17 +0100

    Copy tweaks

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -3,28 +3,27 @@ const projectData = [
     name: 'News Today: Front End',
     link: 'https://news-today-ben-marley.herokuapp.com/',
     description:
-      'Front End for my final full stack solo project for the Northcoders coding bootcamp. Built with React.js and vanilla css.',
+      'Final solo Northcoders project - a Reddit clone, built with React and vanilla css.',
     imageUrl: './projectImages/news-today-fe.png'
   },
   {
     name: 'News Today: Back End',
     link: 'https://ben-nc-news.herokuapp.com/API',
-    description:
-      'RESTful API built with Node.js, Express.js, MongoDB and Mongoose.',
+    description: 'RESTful API built with Node, Express, MongoDB and Mongoose.',
     imageUrl: './projectImages/news-today-be.png'
   },
   {
     name: 'CreditWise',
     link: 'https://github.com/bnmrly/project-game-FE',
     description:
-      'Financial Education Progressive Web App game for Key Stage 2 children. Built in a group of six, with Node.js, React.js, Redux and Firebase.',
+      'Final group project at Northcoders - a Financial Education Progressive Web App game. Built with Node, React, Redux and Firebase.',
     imageUrl: './projectImages/creditWise.png'
   },
   {
     name: 'Google Books API Data Visualisation',
     link: 'https://tranquil-hollows-48024.herokuapp.com/',
     description:
-      'React app that calls the Google Books API to retrieve book data. Built with React.js and react-chartjs-2.',
+      'React app that calls the Google Books API to retrieve book data.',
     imageUrl: './projectImages/google-books-api.png'
   },
   {
@@ -38,7 +37,7 @@ const projectData = [
     name: "Illustrator's website",
     link: 'http://jimnewcombe.co.uk',
     description:
-      'Responsive, custom WordPress site for the London based artist and writer, Jim Newcombe',
+      'Responsive, custom WordPress site for the London based artist, Jim Newcombe',
     imageUrl: './projectImages/jim-newcombe-project.png'
   },
   {
@@ -52,7 +51,7 @@ const projectData = [
     name: 'WordPress Photography Theme',
     link: 'http://benmarleyphotography.co.uk',
     description:
-      'Responsive WordPress site of my film photography, built with the underscores starter theme',
+      'Responsive WordPress site, built with the underscores starter theme',
     imageUrl: './projectImages/bm-photo-project.png'
   }
 ];
