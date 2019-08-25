6b54a356a860f78d6fe82eb4faff942d48f0741b
Ben Marley <benjaminjohnmarley@gmail.com>
2019-08-25 10:59:47 +0100

    Updates

---
+++ precache-manifest.3492267e1d27b551620e1985cb63e571.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9edd637e54e6f1303f32520817ebe8e6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c49d079134e1287ce70e",
+    "url": "/static/js/2.ccd7c319.chunk.js"
+  },
+  {
+    "revision": "6a58c193c9cae817d1b4",
+    "url": "/static/js/main.f0572d5a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
